d73c7f17d62215dec66fb971109d087b6d077f43
hortfrancis <ajhf@hotmail.co.uk>
2024-04-05 02:07:22 +0100

    Receive user speech & return transcription

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 
 const speechToSpeech = require('./controllers/speechToSpeech');
 
 const app = express();
 app.use(cors());
 
+// Multer configuration: receive audio files
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 // Generic hello world endpoint
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.post('/speech-to-speech', upload.single('audio'), speechToSpeech);
 
 module.exports = app;
--- controllers/speechToSpeech.js
+++ controllers/speechToSpeech.js
@@ -1,7 +1,18 @@
 const { transcribeEnglish, transcribeUkrainian } = require("../services/googleCloud");
 
 async function speechToSpeech(req, res) {
-    // TBD
+    try {
+        const userSpeech = req.file.buffer;
+
+        const englishTranscription = await transcribeEnglish(userSpeech);
+
+        console.log("English transcription:", englishTranscription);
+
+        res.status(200).json({ transcription: englishTranscription });
+    } catch (error) {
+        console.error("Error transcribing speech:", error);
+        res.status(500).json({ error: "Error transcribing speech" });
+    }
 }
 
 module.exports = speechToSpeech;
